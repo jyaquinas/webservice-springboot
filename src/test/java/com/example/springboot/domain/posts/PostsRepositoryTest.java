5d94f95a727d8dcb7d4f569c9d285393e03ecbc4
jyaquinas <jythomaskim@gmail.com>
2022-02-28 00:52:47 +0900

    .

--- src/test/java/com/example/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/example/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,6 +1,5 @@
 package com.example.springboot.domain.posts;
 
-import org.assertj.core.error.AssertJMultipleFailuresError;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
