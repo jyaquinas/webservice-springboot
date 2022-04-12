e3536b7e23df8e4024351e9c19bf923b1ae7187d
jyaquinas <jythomaskim@gmail.com>
2022-04-12 22:19:50 +0900

    add profileController

---
+++ src/test/java/com/example/springboot/web/ProfileControllerUnitTest.java
@@ -0,0 +1,47 @@
+package com.example.springboot.web;
+
+import org.junit.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProfileControllerUnitTest {
+    @Test
+    public void get_real_profile(){
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        String profile = controller.profile();
+
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void get_first_if_no_real_profile(){
+        String expectedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("real-db");
+        ProfileController controller = new ProfileController(env);
+
+        String profile = controller.profile();
+
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void get_default_if_no_real_profile(){
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        String profile = controller.profile();
+
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+}
